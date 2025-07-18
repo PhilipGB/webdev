51f73cbcf7d58a3a95f710bc2d0778828138c49a
Philip Burgess <philipgrahamburgess@gmail.com>
2025-07-18 19:10:36 +0100

    netlify form

--- pandc/src/App.jsx
+++ pandc/src/App.jsx
@@ -199,7 +199,7 @@ function App() {
               <h3 className='text-2xl font-bold text-gray-900 dark:text-white mb-4'>
                 Send Us a Message
               </h3>
-              <form className='space-y-4' action='/' netlify>
+              <form className='space-y-4' method='post'>
                 <div>
                   <label
                     htmlFor='name'
